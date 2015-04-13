4f525dc006f27d9359883679f8ea539de4b42652
Ruben De Visscher <ruben.devisscher@observatory.be>
2015-04-13 12:32:13 +0200

    Python 3 compatible prints

--- testcase.py
+++ testcase.py
@@ -5,6 +5,7 @@ import time
 
 for i in range(100):
     with fits.open('test.fits') as hdulist:
-        print i, np.sum(hdulist[1].data)
+        print("{0}: {1}".format(i, np.sum(hdulist[1].data)))
 
+print("Sleeping for 1 minute")
 time.sleep(60)
