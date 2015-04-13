42127046b57d0a6d7efcbe305958b815d945d747
Ruben De Visscher <ruben.devisscher@observatory.be>
2015-04-13 12:29:45 +0200

    Added sleep at the end

--- testcase.py
+++ testcase.py
@@ -1,7 +1,10 @@
 from astropy.io import fits
 #import pyfits as fits
 import numpy as np
+import time
 
 for i in range(100):
     with fits.open('test.fits') as hdulist:
         print i, np.sum(hdulist[1].data)
+
+time.sleep(60)
